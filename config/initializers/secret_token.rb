f1bb1e4577a3bdab95c78de573e837c45fed21b1
SteveClemons <clemons@madisoncollege.edu>
2013-02-24 22:01:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogears::Application.config.secret_token = '0b614e027bed2625ee98b617491dd48481cbd5e94d9a3284167a6a939252851a75e6201ed85046643533e0808109c6aa7269c47a37720eaaaaab7e01e885da26'
